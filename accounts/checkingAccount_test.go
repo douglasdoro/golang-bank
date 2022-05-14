1034d79ccc36dc26538ed384f6985612b15900f3
Douglas Fontes <douglasdoro@gmail.com>
2022-05-14 10:47:30 -0300

    Test created to cover deposit action

--- accounts/checkingAccount_test.go
+++ accounts/checkingAccount_test.go
@@ -16,5 +16,19 @@ func TestShouldReturnTheCurrentBalance(t *testing.T) {
 		balance:       100,
 	}
 
-	assert.Equal(t, float64(100), cc.GetBalance(), "Balance should be equal 100")
+	assert.Equal(t, float64(100), cc.GetBalance(), "The balance should be equal to 100")
+}
+
+func TestShouldReturnNewBalanceValueAfterDeposit(t *testing.T) {
+	checkingAccount := ContaCorrente{
+		Client:        clients.Client{},
+		AgencyNumber:  0001,
+		AccountNumber: 1234,
+		balance:       100,
+	}
+
+	checkingAccount.Depositar(100.0)
+	newBalance := checkingAccount.GetBalance()
+
+	assert.Equal(t, 200.0, newBalance, "The balance should be equal to 200.0")
 }
